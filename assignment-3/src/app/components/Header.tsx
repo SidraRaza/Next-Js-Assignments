0df87cbd03bee5e22bbd0a802c5b82a6184bc85f
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-10-17 22:13:53 +0500

    Update Header.tsx

--- assignment-3/src/app/components/Header.tsx
+++ assignment-3/src/app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='bg-purple-400 p-6 '>
      <div className='flex flex-col-1 justify-end lg:gap-4 gap-0  '>
         <div>
-        <h1 className='text-white lg:mr-[750px] mr-2 text-2xl ml-3 '>Porfolio</h1>
+        <h1 className='text-white lg:mr-[750px] mr-2 text-2xl ml-3 pl-2'>Porfolio</h1>
         </div>
      <Link href="#" className='text-white ml-3 sm:text-1xl rounded-lg hover:bg-purple-200 hover:text-black border-1 border-white  lg:px-4 px-2 py-2   '>Home</Link>
       <Link href="#" className='text-white sm:text-1xl rounded-lg hover:bg-purple-200 border-1 hover:text-black  border-white  lg:px-4 px-2 py-2   '>About</Link>
