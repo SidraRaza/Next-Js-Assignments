7e7862485e0ba6275e8f395d83f1e2cbd9cfa5ae
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-10-19 09:39:29 +0500

    first commit

---
+++ one/src/app/About/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import About from '../components/About';
+
+const Page = () => {
+  return (
+ <About/> 
+  )
+};
+
+export default Page;
---
+++ one/src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-purple-500 p-5'>
+        <h2 className='text-center text-white'>Created by Sidra Raza</h2>
+        <p className='text-center text-white'>Copyright &copy; 2023. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
