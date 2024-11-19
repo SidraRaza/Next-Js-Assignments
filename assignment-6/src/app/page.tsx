92179667b249a6a5631ff9b4af4a396f73d43e99
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-11-19 11:52:27 +0500

    first commit

--- assignment-6/src/app/page.tsx
+++ assignment-6/src/app/page.tsx
@@ -5,7 +5,7 @@ import Logo from '@/components/Logo'
 import MinHeader from '@/components/MinHeader'
 import Team from '@/components/Team'
 import React from 'react'
-import HeroTest from '@/components/HeroTest'
+
 const page = () => {
   return (
     <div>
@@ -15,7 +15,6 @@ const page = () => {
       <Logo/>
       <Team/>
       <Achievements/>
-      {/* <HeroTest/> */}
     </div>
   )
 }
