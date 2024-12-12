918db335e18dea5ed740999a11ea58337e7078bd
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-12-12 06:25:18 +0500

    first commit

--- hackhathon/src/components/Hero.tsx
+++ hackhathon/src/components/Hero.tsx
@@ -4,15 +4,22 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className='lg:w-[1439px] lg:h-[716px]  w-md hidden md:block'>
-        <Image
-            src={'/h11.svg'}
-            alt="img"
-            width={1439}
-            height={716}
-            className="lg:w-[1439px] h-[716px] w-md"
-        />
+
+<div className='relative lg:w-[1439px] lg:h-[716px]  w-md hidden md:block'>
+  <img src='/shop-hero.svg' alt='img' className='absolute inset-0 w-full h-full object-cover' />
+  <div className='absolute left-[100px] top-0 w-[50%] h-full flex flex-col justify-center text-white gap-[35px] p-[40px]'>
+    <h1 className='text-[16px] leading-[24px] font-bold'>SUMMER 2020</h1>
+    <h1 className='text-[58px] leading-[80px] font-bold'>NEW COLLECTION</h1>
+    <p className='text-[20px] leading-[30px] w-[376px] h-[60px]'>
+      We know how large objects will act, but things on a small scale.
+    </p>
+    <div className='w-[221px] h-[62px] flex justify-center items-center text-white bg-[#2DC071]'>
+      <button className='text-[24px] leading-[32px] font-bold'>Shop Now</button>
     </div>
+  </div>
+</div>
+
+  
     <div className='w-[414px] h-[753px] md:hidden'>
     <Image
             src={'/h12.svg'}
